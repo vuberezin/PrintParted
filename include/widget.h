809d6ab8098628211e5a8f90d60e5902881f3040
Vadim Berezin <32239661+vuberezin@users.noreply.github.com>
2021-08-11 11:48:24 +0300

    Create widget.h

---
+++ include/widget.h
@@ -0,0 +1,98 @@
+/*
+ * PrintParted is a program for viewing table of disk partitions
+ * Copyright (C) 2021  Vadim Berezin
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+
+#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <QWidget>
+#include <QTableView>
+#include <QListView>
+#include <QLabel>
+#include <QLineEdit>
+#include <QTextBrowser>
+#include <QDateEdit>
+#include <QString>
+#include <QStringListModel>
+#include <QComboBox>
+#include <QMessageBox>
+#include <QHeaderView>
+#include <QFormLayout>
+#include <QItemSelectionModel>
+#include <QProgressBar>
+#include <iostream>
+#include <unistd.h>
+#include "parttablemodel.h"
+#include "comboboxmodel.h"
+#include "disk.h"
+#include "chart.h"
+#include "disktablemodel.h"
+#include "dialog.h"
+#include "freespace.h"
+#include "freespacemodel.h"
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class Widget; }
+QT_END_NAMESPACE
+
+
+class TableModel;
+class Widget : public QWidget
+{
+Q_OBJECT
+
+public:
+    Widget(QWidget *parent = 0);
+
+    QTableView *tableView;
+    QTableView *diskTableView;
+    TableModel *tableModel;
+    QTableView *freespaceView;
+    QLabel *diskLabel;
+    QStringListModel *model;
+    QComboBox *comboBox;
+    QList<QString> *values;
+    ComboBoxModel *combomodel;
+    DiskData *diskData;
+    DiskTableModel *diskTableModel;
+    DataChart *dataChart;
+    DataParted *dataParted;
+    DataFreespace *dataFreespace;
+    FreespaceModel * freespaceModel;
+    QChartView *chartView;
+    Dialog *dialog;
+    QDialog* dialogWidget;
+    QGridLayout *layout;
+    int vecSize(vector<vector<string>> vec);
+    QList<QString>* addComboItem(DiskData *diskData, QList<QString>* values);
+    void createSplash(QString str);
+
+    ~Widget();
+
+private:
+     Ui::Widget *ui;
+
+public slots:
+    void clickedAction( int i);
+
+
+};
+
+
+#endif // WIDGET_H
