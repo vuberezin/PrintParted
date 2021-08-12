e60d3f1861f1781694f3f6e2e6aa8d2e3ba49e7c
Vadim Berezin <32239661+vuberezin@users.noreply.github.com>
2021-08-12 23:55:10 +0300

    Update building.sh

--- scripts/building.sh
+++ scripts/building.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/bash
+
 g++ -c -pipe -g -std=gnu++11 -Wall -W -D_REENTRANT -fPIC -DQT_DEPRECATED_WARNINGS -DQT_QML_DEBUG -DQT_CHARTS_LIB -DQT_WIDGETS_LIB 
 -DQT_GUI_LIB -DQT_CORE_LIB -I../PrintParted -I. -isystem /usr/include/x86_64-linux-gnu/qt5 -isystem /usr/include/x86_64-linux-gnu/qt5/QtCharts 
 -isystem /usr/include/x86_64-linux-gnu/qt5/QtWidgets -isystem /usr/include/x86_64-linux-gnu/qt5/QtGui -isystem /usr/include/x86_64-linux-gnu/qt5/QtCore
