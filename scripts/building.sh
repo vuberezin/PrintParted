ca686e2ce398b08a999da0061ce9213478e4425a
Vadim Berezin <32239661+vuberezin@users.noreply.github.com>
2021-08-12 23:54:56 +0300

    Update building.sh

--- scripts/building.sh
+++ scripts/building.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 g++ -c -pipe -g -std=gnu++11 -Wall -W -D_REENTRANT -fPIC -DQT_DEPRECATED_WARNINGS -DQT_QML_DEBUG -DQT_CHARTS_LIB -DQT_WIDGETS_LIB 
--DQT_GUI_LIB -DQT_CORE_LIB -I../QPrintParted3.1 -I. -isystem /usr/include/x86_64-linux-gnu/qt5 -isystem /usr/include/x86_64-linux-gnu/qt5/QtCharts 
+-DQT_GUI_LIB -DQT_CORE_LIB -I../PrintParted -I. -isystem /usr/include/x86_64-linux-gnu/qt5 -isystem /usr/include/x86_64-linux-gnu/qt5/QtCharts 
 -isystem /usr/include/x86_64-linux-gnu/qt5/QtWidgets -isystem /usr/include/x86_64-linux-gnu/qt5/QtGui -isystem /usr/include/x86_64-linux-gnu/qt5/QtCore
--I. -I. -I/usr/lib/x86_64-linux-gnu/qt5/mkspecs/linux-g++ -o main.o ../QPrintParted3.1/main.cpp
+-I. -I. -I/usr/lib/x86_64-linux-gnu/qt5/mkspecs/linux-g++ -o main.o ../PrintParted/main.cpp
