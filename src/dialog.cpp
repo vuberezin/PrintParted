b64f468345b00179a50a069e141484259d7db47e
Vadim Berezin <32239661+vuberezin@users.noreply.github.com>
2021-08-25 21:02:57 +0300

    Update dialog.cpp

--- src/dialog.cpp
+++ src/dialog.cpp
@@ -28,24 +28,34 @@ Dialog::Dialog(QWidget *parent, DataParted *dataParted, DiskData *diskdata, Data
     QGridLayout *layout = new QGridLayout;
     dataChart = new DataChart( dataParted, diskdata, dataFreespace, index);
     chartView = new QChartView();
+    diskTableModel = new DiskTableModel;
+    diskTableView = new QTableView;
+    diskTableView->setModel(diskTableModel);
+    diskTableView->setContextMenuPolicy(Qt::CustomContextMenu);
+
+    diskTableView->horizontalHeader()->setStyleSheet("QHeaderView::section {background-color: #F0E68C }");
+    diskTableView->setStyleSheet("QTableView:item:selected {background-color: #F0E68C  ; color: #000000}\n"
+                                 "QTableView:item:selected:focus {background-color: #F0E68C;}");
+    diskTableView->horizontalHeader()->setSectionResizeMode(QHeaderView::Stretch);
     label = new QLabel;
     label->setWordWrap(true);
-    label->setText("Disk: " + QString::fromStdString(diskdata->vecSave.at(index).at(0)) +
-                    "\n" + "Model: " + QString::fromStdString(diskdata->vecSave.at(index).at(1)) +
-                    "\n" + "Type: " + QString::fromStdString(diskdata->vecSave.at(index).at(2)) +
-                    "\n" + "Size: " + QString::fromStdString(diskdata->vecSave.at(index).at(3)) +
-                    "\n" + "Logical sector size: " + QString::fromStdString(diskdata->vecSave.at(index).at(4)) + "B" +
-                    "\n" + "Physical sector size: " + QString::fromStdString(diskdata->vecSave.at(index).at(5)) + "B" +
-                    "\n" + "Partition Table: " + QString::fromStdString(diskdata->vecSave.at(index).at(6)) +
-                    "\n" + "Disk Flags: " + QString::fromStdString(diskdata->vecSave.at(index).at(7)));
+    label->setText("  Disk: " + QString::fromStdString(diskdata->vecSave.at(index).at(0)) +
+                    "\n" + "  Model: " + QString::fromStdString(diskdata->vecSave.at(index).at(1)) +
+                    "\n" + "  Type: " + QString::fromStdString(diskdata->vecSave.at(index).at(2)) +
+                    "\n" + "  Size: " + QString::fromStdString(diskdata->vecSave.at(index).at(3)) +
+                    "\n" + "  Logical sector size: " + QString::fromStdString(diskdata->vecSave.at(index).at(4)) + "B" +
+                    "\n" + "  Physical sector size: " + QString::fromStdString(diskdata->vecSave.at(index).at(5)) + "B" +
+                    "\n" + "  Size in sectors: " + QString::fromStdString(diskdata->vecSave.at(index).at(6))  +
+                    "\n" + "  Partition Table: " + QString::fromStdString(diskdata->vecSave.at(index).at(7)) +
+                    "\n" + "  Disk Flags: " + QString::fromStdString(diskdata->vecSave.at(index).at(8)));
     chartView->setRenderHint(QPainter::Antialiasing);
     chartView->chart()->setTitle("Patitions table");
     chartView->chart()->addSeries(dataChart->series);
     chartView->chart()->legend()->setAlignment(Qt::AlignBottom);
     chartView->chart()->setTheme(QChart::ChartThemeQt);
     chartView->chart()->legend()->setFont(QFont("Arial", 10));
-    layout->addWidget(chartView, 0, 0);
-    layout->addWidget(label, 0, 1);
+    layout->addWidget(chartView, 0, 0, 1, 1);
+    layout->addWidget(label, 1, 0, 2, 1);
     setLayout(layout);
 
 };
